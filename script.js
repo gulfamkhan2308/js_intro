d8d89b129b2601f36ba344278f743af8880eb5df
gulfamkhan2308 <gulfamkhan2308@gmail.com>
2025-02-24 22:31:54 +0500

    assignment 2 added

---
+++ script.js
@@ -0,0 +1,12 @@
+let a = [1, 2, 3, 4]; 
+ console.log(a)
+
+ let colors = ["red", "green", "blue"]; 
+ console.log(colors);
+
+
+
+document.getElementById("consoleButton").addEventListener("click", function() {
+    alert("Press F12 or Ctrl+Shift+I to open the console.");
+    console.log("[1, 2, 3, 4]");
+});
